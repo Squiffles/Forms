6f1e6b4dd72738a8303efbccf3eb38e0814aed2d
SebastianMartel <martel.ssebastian@gmail.com>
2023-09-25 18:47:09 -0500

    first commit, basic setup

---
+++ CLIENT/src/App.tsx
@@ -0,0 +1,18 @@
+// --------------- IMPORTS ---------------
+import './App.css'
+
+
+// --------------- COMPONENT ---------------
+function App() {
+
+    // COMPONENT:
+    return (
+        <main className="bg-black min-w-full">
+            
+        </main>
+    );
+};
+
+
+// --------------- EXPORTS ---------------
+export default App;
