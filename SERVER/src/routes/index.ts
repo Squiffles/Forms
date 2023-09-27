838b86caac65ed5238994f3dce7b310d3aba112e
SebastianMartel <martel.ssebastian@gmail.com>
2023-09-27 15:32:23 -0500

    deploy fix

--- SERVER/src/routes/index.ts
+++ SERVER/src/routes/index.ts
@@ -1,5 +1,5 @@
 // --------------- IMPORTS ---------------
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 import answerRouter from "./Answer";
 import userRouter from "./User";
 
@@ -7,11 +7,18 @@ import userRouter from "./User";
 // --------------- ROUTER ---------------
 const indexRouter = Router();
 
+const initialRequest = async (_req: Request, res: Response) => {
+    try {
+        res.status(200).send("It's working.");
+    } catch (error) {
+        res.status(500).send("error")
+    };
+};
+
+
 indexRouter.use("/answer", answerRouter);
 indexRouter.use("/user", userRouter);
-indexRouter.get("/", ((_req, res) => {
-    return res.status(200).send("It's working.");
-}));
+indexRouter.get("/", initialRequest);
 
 
 // --------------- EXPORTS ---------------
